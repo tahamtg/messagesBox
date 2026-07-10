7a61626f7def4a79a8bc5a25a9d6b65d872fd5b0
taha <tahaghataei@gmail.com>
2026-07-11 03:16:05 +0330

    test asgi

--- chatflow_backend/massage/asgi.py
+++ chatflow_backend/massage/asgi.py
@@ -6,7 +6,10 @@ from channels.auth import AuthMiddlewareStack
 from massage.routing import websocket_urlpatterns
 from massage.middleware import JWTAuthMiddleware
 
+print("ASGI LOADED")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "massagebox.settings")
+
 
 django_asgi_app = get_asgi_application()
 
