f2453b30c76d3212ee654096db8ce7230be6d307
taha <tahaghataei@gmail.com>
2026-07-11 03:42:35 +0330

    try again and rm replace asgi.py to one

--- chatflow_backend/massagebox/asgi.py
+++ chatflow_backend/massagebox/asgi.py
@@ -1,17 +1,18 @@
-"""
-ASGI config for massagebox project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-print("ASGI FILE LOADED")
-
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "massagebox.settings")
+
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'massagebox.settings')
+from massage.routing import websocket_urlpatterns
+from massage.middleware import JWTAuthMiddleware
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": JWTAuthMiddleware(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
