667010f8d4352278e8253712400e4b8a496ac4b6
taha <tahaghataei@gmail.com>
2026-07-11 15:01:47 +0330

    change asgi class JWT

--- chatflow_backend/massagebox/asgi.py
+++ chatflow_backend/massagebox/asgi.py
@@ -12,7 +12,8 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": JWTAuthMiddleware(
-        URLRouter(websocket_urlpatterns)
+
+    "websocket": URLRouter(
+        websocket_urlpatterns
     ),
 })
