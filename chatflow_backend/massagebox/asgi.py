9a4af3a487f6f311e6847f7b2d05d4af2ad39782
taha <tahaghataei@gmail.com>
2026-07-11 18:57:48 +0330

    rewrite nginx

--- chatflow_backend/massagebox/asgi.py
+++ chatflow_backend/massagebox/asgi.py
@@ -8,7 +8,7 @@ django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from massage.routing import websocket_urlpatterns
-
+print("ASGI STARTED")
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": URLRouter(websocket_urlpatterns),
