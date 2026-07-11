30ea890a27671d420f0e011272cbd93542759843
taha <tahaghataei@gmail.com>
2026-07-11 15:06:30 +0330

    change asgi

--- chatflow_backend/massagebox/asgi.py
+++ chatflow_backend/massagebox/asgi.py
@@ -3,17 +3,19 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "massagebox.settings")
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 from massage.routing import websocket_urlpatterns
 from massage.middleware import JWTAuthMiddleware
+
 print("ASGI LOADED")
-django_asgi_app = get_asgi_application()
+print(websocket_urlpatterns)
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-
-    "websocket": URLRouter(
-        websocket_urlpatterns
+    "websocket": JWTAuthMiddleware(
+        URLRouter(websocket_urlpatterns)
     ),
 })
