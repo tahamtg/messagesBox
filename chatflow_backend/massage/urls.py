e51ef349ce1965061e2862aaf2b10b6dba8b9000
taha <tahaghataei@gmail.com>
2026-07-17 19:01:36 +0330

    fix urls

--- chatflow_backend/massage/urls.py
+++ chatflow_backend/massage/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
+from .views import CreateTokenCookie
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 
 urlpatterns = [
-    path('sign-up/', views.Sign_Up, name= 'sign up users'),
-    path('login/', CreateTokenCookie.as_view(), name= 'login users'),
-    path('check-auth/', views.Check_Auth, name='athuneticate users'),
+    path('sign-up/', views.Sign_Up, name='sign up users'),
+    path('login/', CreateTokenCookie.as_view(), name='login users'),
+    path('check-auth/', views.Check_Auth, name='authenticate users'),
     path('logout/', views.LogOut, name='logout users'),
-]   
-
-
+]
