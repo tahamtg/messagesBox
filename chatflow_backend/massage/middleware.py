35d27eb2d98f70e076c10d8f8a18f9fef0164702
taha <tahaghataei@gmail.com>
2026-07-17 18:50:14 +0330

    fix middleware

--- chatflow_backend/massage/middleware.py
+++ chatflow_backend/massage/middleware.py
@@ -1,49 +1,51 @@
 from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.exceptions import TokenError
+from .models import User_Account
+
 
 class JWTAuthMiddleware:
 
     def __init__(self, app):
         self.app = app
 
-
     async def __call__(self, scope, receive, send):
-        from django.contrib.auth.models import AnonymousUser
-        from rest_framework_simplejwt.tokens import AccessToken
-        from .models import User_Account
 
         scope["user"] = AnonymousUser()
 
         if scope["type"] == "websocket":
 
-            header = dic(scope['headers'])
-            cookie = header.get(b"cookie")
-            cookie = cookie.decode()
+            headers = dict(scope["headers"])
+
+            cookie = headers.get(b"cookie")
+
             if not cookie:
                 return await self.app(scope, receive, send)
-            cookies ={}
-            for items in cookie.split(";"):
-                key, value = cookie.strip().split('=', 1)
+
+            cookie = cookie.decode()
+
+            cookies = {}
+
+            for item in cookie.split(";"):
+                key, value = item.strip().split("=", 1)
                 cookies[key] = value
-            token = cookies["access"]
 
-            try:
+            token = cookies.get("access")
 
-                access = token.AccessToken(token)
-            
-            except TokenError:
+            if token:
+                try:
+                    access = AccessToken(token)
 
-                scope["user"] = AnonymousUser()
-                return await self.app(scope, receive, send)
-            
-            user_id = access["user_id"]
+                    user_id = access["user_id"]
 
-            userid = database_sync_to_async(User_Account.get)(
-                id=user_id
-            )
+                    user = await database_sync_to_async(
+                        User_Account.objects.get
+                    )(id=user_id)
 
-            scope["user"] = userid
+                    scope["user"] = user
 
-
+                except TokenError:
+                    scope["user"] = AnonymousUser()
 
         return await self.app(scope, receive, send)
